9fa296bab9f4b7b0444653f0d583a16c6a5a4e94
alan7s <alan7@outlook.com>
2024-04-06 14:35:14 -0300

    Update HomeFragment.java

--- app/src/main/java/com/example/mvp_inicient01/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/mvp_inicient01/ui/home/HomeFragment.java
@@ -1,14 +1,18 @@
 package com.example.mvp_inicient01.ui.home;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
-import android.os.TokenWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -24,10 +28,7 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 import com.karumi.dexter.Dexter;
 import com.karumi.dexter.PermissionToken;
 import com.karumi.dexter.listener.PermissionDeniedResponse;
@@ -35,74 +36,122 @@ import com.karumi.dexter.listener.PermissionGrantedResponse;
 import com.karumi.dexter.listener.PermissionRequest;
 import com.karumi.dexter.listener.single.PermissionListener;
 
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
+    private SupportMapFragment supportMapFragment;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+    private double speed = 0.0;
+    private double currentSpeed, kmphSpeed;
 
-    SupportMapFragment supportMapFragment;
-    FusedLocationProviderClient fusedLocationProviderClient;
+    public static double round(double unrounded, int precision, int roundingMode) {
+        BigDecimal bd = new BigDecimal(unrounded);
+        BigDecimal rounded = bd.setScale(precision, roundingMode);
+        return rounded.doubleValue();
+    }
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
         getActivity().getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         supportMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getActivity());
 
-        fusedLocationProviderClient = (FusedLocationProviderClient) LocationServices.getFusedLocationProviderClient(getActivity());
+        requestLocationPermission();
 
-        Dexter.withContext(getActivity().getApplicationContext()).withPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+        return root;
+    }
+
+    private void requestLocationPermission() {
+        Dexter.withContext(getActivity().getApplicationContext())
+                .withPermission(Manifest.permission.ACCESS_FINE_LOCATION)
                 .withListener(new PermissionListener() {
                     @Override
                     public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
-                        getCurrentLocation();
+                        initLocationListener();
                     }
 
                     @Override
-                    public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
-
-                    }
+                    public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {}
 
                     @Override
                     public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
                         permissionToken.continuePermissionRequest();
                     }
                 }).check();
-
-        return root;
     }
 
-    public void getCurrentLocation() {
-        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
+    private void initLocationListener() {
+        locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(@NonNull Location location) {
+                updateMapLocation(location);
+            }
+
+            @Override
+            public void onProviderEnabled(@NonNull String provider) {}
+
+            @Override
+            public void onProviderDisabled(@NonNull String provider) {}
+        };
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             return;
         }
-        Task<Location> task = fusedLocationProviderClient.getLastLocation();
-        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+    }
+
+    private void updateSpeedDisplay(Location location) {
+        double speed = location.getSpeed();
+        double currentSpeed = round(speed, 3, BigDecimal.ROUND_HALF_UP);
+        double kmphSpeed = round((currentSpeed * 3.6), 3, BigDecimal.ROUND_HALF_UP);
+        //Log.d("speed", Double.toString(kmphSpeed));
+        TextView timeTextView = getActivity().findViewById(R.id.timeTextView);
+        TextView speedTextView = getActivity().findViewById(R.id.speedTextView);
+        TextView longitudeTextView = getActivity().findViewById(R.id.longitudeTextView);
+        TextView latitudeTextView = getActivity().findViewById(R.id.latitudeTextView);
+
+        SimpleDateFormat formatDate = new SimpleDateFormat("dd-MMM-yyyy HH:mm:ss.SSS");
+        String formattedTime = formatDate.format(new Date(location.getTime()));
+
+        double longitude = location.getLongitude();
+        double latitude = location.getLatitude();
+        DecimalFormat df = new DecimalFormat("#.########");
+
+        String speedText = kmphSpeed + " km/h";
+        String longitudeText = df.format(longitude);
+        String latitudeText = df.format(latitude);
+
+        speedTextView.setText(speedText);
+        timeTextView.setText(formattedTime);
+        longitudeTextView.setText(longitudeText);
+        latitudeTextView.setText(latitudeText);
+    }
+    private void updateMapLocation(Location location) {
+        supportMapFragment.getMapAsync(new OnMapReadyCallback() {
             @Override
-            public void onSuccess(Location location) {
-                supportMapFragment.getMapAsync(new OnMapReadyCallback() {
-                    @Override
-                    public void onMapReady(@NonNull GoogleMap googleMap) {
-                        if(location != null){
-                            LatLng latLng = new LatLng(location.getLatitude(),location.getLongitude());
-                            MarkerOptions markerOptions = new MarkerOptions().position(latLng).title("Current Location!");
-                            googleMap.addMarker(markerOptions);
-                            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,15));
-                        } else {
-                            Toast.makeText(getActivity(), "Please on Your Location App Permissions", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
+            public void onMapReady(@NonNull GoogleMap googleMap) {
+                if (location != null) {
+                    LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                    MarkerOptions markerOptions = new MarkerOptions().position(latLng).title("Current Location!");
+                    googleMap.clear();
+                    googleMap.addMarker(markerOptions);
+                    googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
+                    updateSpeedDisplay(location);
+                } else {
+                    Toast.makeText(getActivity(), "Please turn on Your Location App Permissions", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
