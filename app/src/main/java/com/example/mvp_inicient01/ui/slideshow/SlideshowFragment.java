224d0c9e77eb45d1c1b31f4d82fff3f3e6dce36c
alan7s <alan7@outlook.com>
2024-08-08 19:40:28 -0300

    Update SlideshowFragment.java

--- app/src/main/java/com/example/mvp_inicient01/ui/slideshow/SlideshowFragment.java
+++ app/src/main/java/com/example/mvp_inicient01/ui/slideshow/SlideshowFragment.java
@@ -1,6 +1,7 @@
 package com.example.mvp_inicient01.ui.slideshow;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
