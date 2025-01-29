b31b860362b956fac32ba9d3747a58d6725cb7d1
alan7s <alan7@outlook.com>
2025-01-29 13:03:23 -0300

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,8 @@ dependencies {
     implementation("com.google.android.gms:play-services-location:21.0.1")
     implementation("com.karumi:dexter:6.2.1")
     implementation("com.dropbox.core:dropbox-core-sdk:6.1.0")
+    implementation("com.dropbox.core:dropbox-android-sdk:6.1.0")
+
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
